3ca743f4dad9162cbb455389618007e16ca21f7d
GitubUser87 <153625913+GitubUser87@users.noreply.github.com>
2024-12-16 16:54:55 +0000

    Update 16/12/2024
    Got items in the plugin.

--- InventoryCpp/Intermediate/Build/BuildRulesProjects/InventoryCppModuleRules/obj/Development/InventoryCppModuleRules.AssemblyInfo.cs
+++ InventoryCpp/Intermediate/Build/BuildRulesProjects/InventoryCppModuleRules/obj/Development/InventoryCppModuleRules.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InventoryCppModuleRules")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Development")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3548605cb72aabb8ec97576b73fb5105a21d1c76")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a60fd51edd365956e62c85db4b93a92f431f50d0")]
 [assembly: System.Reflection.AssemblyProductAttribute("InventoryCppModuleRules")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InventoryCppModuleRules")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ InventoryCpp/Plugins/Inventory/Source/Inventory/Private/MyItem.cpp
@@ -0,0 +1,68 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "MyItem.h"
+
+#include "Components/BoxComponent.h"
+
+// Sets default values
+AMyItem::AMyItem()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	//ItemMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Root"));
+	//RootComponent = ItemMesh;
+
+	//Box = CreateDefaultSubobject<UBoxComponent>(FName("Box"));
+	//Box->SetBoxExtent(FVector(74.0f, 125.0f, 100.0f));
+	//Box->SetupAttachment(ItemMesh);
+	//Box->SetRelativeLocation(FVector(0.0f, 0.0f, 100.0f));
+}
+
+void AMyItem::OverlapBegin(UPrimitiveComponent* OverlappedComponent, AActor* OtherActor, UPrimitiveComponent* OtherComp, int32 OtherBodyIndex, bool bFromSweep, const FHitResult& SweepResult)
+{
+	//AInventoryCppCharacter* tempActor = Cast<AInventoryCppCharacter>(OtherActor);
+
+	//if (IsValid(tempActor))
+	//{
+	//	UE_LOG(LogTemp, Warning, TEXT("Overlapped!"));
+	//	if (tempActor->GetInventory()->CheckMaxWeight(GetWeight()))
+	//	{
+	//		UE_LOG(LogTemp, Warning, TEXT("Warning Inventory Full!"));
+	//	}
+	//	else
+	//	{
+	//		tempActor->GetInventory()->AddItems(itemInfo);
+	//		Destroy();
+	//	}
+
+	//	return;
+
+
+	//}
+	//else
+	//{
+	//	UE_LOG(LogTemp, Warning, TEXT("Not the player!"));
+	//}
+}
+
+// Called when the game starts or when spawned
+void AMyItem::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+float AMyItem::GetWeight()
+{
+	return itemInfo2.weight;
+}
+
+// Called every frame
+void AMyItem::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
