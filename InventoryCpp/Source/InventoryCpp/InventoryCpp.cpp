e0d8824e0ac50689e1c9d3d61887dc908b4eb5e8
GitubUser87 <153625913+GitubUser87@users.noreply.github.com>
2026-04-30 12:29:20 +0100

    Push

--- InventoryCpp/Source/InventoryCpp/InventoryCpp.cpp
+++
@@ -1,7 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "InventoryCpp.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, InventoryCpp, "InventoryCpp" );
- 
